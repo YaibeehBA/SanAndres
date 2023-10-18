fb1b470244c7f4929e8925beee0f1b96626838ce
stalyn <smtihsly@gmail.com>
2023-10-18 11:40:50 -0500

    separacion del index en encabezado, footer y links

--- index.php
+++ index.php
@@ -4,12 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <?php require('inc/links.php') ?>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="css/comun.css">
-
+   
     <title>San Andres</title>
     <style>
         .availability-form {
@@ -31,142 +28,7 @@
 </head>
 
 <body class="bg-light">
-    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="#index.php">BB San Andres</a>
-            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Hospedaje</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Guía Turístico</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Bar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Contáctanos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="#">Nosotros</a>
-                    </li>
-
-
-                </ul>
-                <div class="d-flex">
-                    <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-                        Iniciar Sesión
-                    </button>
-                    <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">
-                        Registrarse
-                    </button>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <!-- loginModal -->
-    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <form>
-                    <div class="modal-header">
-                        <h5 class="modal-title d-flex align-items-center">
-                            <i class="bi bi-person-circle fs-3 me-2"></i>Iniciar Sesión
-                        </h5>
-                        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label class="form-label">Correo Electrónico</label>
-                            <input type="email" class="form-control shadow-none">
-                        </div>
-                        <div class="mb-4">
-                            <label class="form-label">Contraseña</label>
-                            <input type="password" class="form-control shadow-none">
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-2">
-                            <button type="submit" class="btn btn-dark shadow-none">Iniciar Sesión</button>
-                            <a href="javascript: void(0)" class="text-secondary text-decoration-none">Olvide mi contraseña? </a>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <!-- registerModal -->
-    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <form>
-                    <div class="modal-header">
-                        <h5 class="modal-title d-flex align-items-center">
-                            <i class="bi bi-person-lines-fill fs-3 me-2"></i>
-                            Registro de usuario
-                        </h5>
-                        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-                            Nota: Sus datos deben coincidir con su identificación (Nombres, celular,correo, etc)
-                            que se solicitará al registrarse
-                        </span>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Nombre</label>
-                                    <input type="text" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Correo Electrónico</label>
-                                    <input type="email" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Número de celular</label>
-                                    <input type="number" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Foto</label>
-                                    <input type="file" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-12 p-0 mb-3">
-                                    <label class="form-label">Dirección</label>
-                                    <textarea class="form-control shadow-none" rows="1"></textarea>
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label ">Pincode</label>
-                                    <input type="number" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Fecha de cumpleaños</label>
-                                    <input type="date" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label ">Contraseña</label>
-                                    <input type="password" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Confirme su contraseña</label>
-                                    <input type="password" class="form-control shadow-none">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="text-center my-1">
-                            <button type="submit" class="btn btn-dark shadow-none">Registrarse</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+   <?php require('inc/header.php') ?>
 
     <!-- Carousel -->
     <div class="container-fluid px-lg-4 mt-4">
@@ -394,7 +256,7 @@
                             </span>
 
                         </div>
-                        <div class="d-flex justify-content-evenly mb-2">
+                        <div class="d-flex justify-content-evenly mb-2 ">
                             <a href="#" class="btn btn-sm text-white custon-bg shadow-none">Reservar ahora</a>
                             <a href="#" class="btn btn-sm btn-outline-dark shadow-none">Mas detalles</a>
 
@@ -604,44 +466,8 @@
     </div>
 
     <!-- footer-->
-    <div class="container-fluid bg-white mt-5">
-        <div class="row">
-            <div class="col-lg-4 p-4">
-                <h3 class="h-font fw-bold fs-3 mb-2">BB SAN ANDRES</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Omnis at debitis voluptatibus aut laboriosam.
-                    Quam, eos! Amet nesciunt sed inventore similique veniam rerum saepe repudiandae beatae.
-                    Alias maiores tempore illum?
-                </p>
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Enlaces</h5>
-                <a href="#" class="d-inline-block mb-2 text-dark  text-decoration-none">Home</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark  text-decoration-none">Hospedaje</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark  text-decoration-none">Guias turiticos</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark  text-decoration-none">Bar</a><br>
-                <a href="#" class="d-inline-block mb-2 text-dark  text-decoration-none">Nosotros</a><br>
-
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Siguenos</h5>
-                <a href="# " class="d-inline-block text-dark text-decaration-none mb-2">
-                    <i class="bi bi-twitter-x me-1"></i>Twitter
-                </a><br>
-                <a href="# " class="d-inline-block text-dark text-decaration-none mb-2">
-                    <i class="bi bi-facebook me-1"></i>Facebook
-                </a><br>
-                <a href="# " class="d-inline-block text-dark text-decaration-none ">
-                    <i class="bi bi-instagram me-1"></i>Instagram
-                </a><br>
-            </div>
-        </div>
-
-    </div>
-
-    <h6 class="text-center bg-dark text-white  p-3 m-0">Desarrollado por Stalyn y Gitur-ESPOCH</h6>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <?php require('inc/footer.php') ?>
+    
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script>
         var swiper = new Swiper(".swiper-container", {
